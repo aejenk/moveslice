02a6695f6b6635ae59e5e6b778ccf64472c0180e
Calmynt <mctech26@gmail.com>
2019-08-03 14:17:52 +0200

    Added docs and updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,31 @@
-//! This crate contains a single function `moveslice`. Its purpose 
-//! is to move a chunk within a slice around. It only uses safe functions,
-//! and acts efficiently by using the 
-//! [`split_at_mut`](https://doc.rust-lang.org/std/primitive.slice.html#method.split_at_mut)
-//! and 
-//! [`rotate_left`](https://doc.rust-lang.org/std/primitive.slice.html#method.rotate_left)/
-//! [`rotate_right`](https://doc.rust-lang.org/std/primitive.slice.html#method.rotate_right)
-//! functions.
+#![no_std]
+//! This crate contains functionality to move a slice within an array around.
+//! It only uses safe functions, and acts efficiently by using the
+//! [`split_at_mut`][split-at-mut] and
+//! [`rotate_left`][rotate-left]/[`rotate_right`][rotate-right] functions.
+//! 
+//! This crate also has a focus on being `no_std`, to allow this functionality
+//! in all case where it is required.
+//! 
+//! The main feature this crate provides is implementing `moveslice` functions
+//! for any and all slices/arrays. Note that this only works for slices - vectors
+//! and other structures would need to be converted into a slice before using this function.
 //! 
 //! # Examples:
 //! 
 //! ```
-//! use moveslice::moveslice;
+//! use moveslice::Moveslice;
 //! 
 //! let mut arr = [1,2,3,4,5,6,7,8,9];
 //! 
 //! // The following moves the slice 3..6 to index 1.
 //! // In effect, it moves [4,5,6] over to where [2] is.
-//! moveslice(&mut arr, (3,6), 1);
+//! arr.moveslice(3..6, 1);
 //! assert_eq!(arr, [1,4,5,6,2,3,7,8,9]);
 //! 
 //! // The following moves the slice 3..6 to index 6.
 //! // In effect, it moves [6,2,3] over to where [7] is.
-//! moveslice(&mut arr, (3,6), 6);
+//! arr.moveslice(3..6, 6);
 //! assert_eq!(arr, [1,4,5,7,8,9,6,2,3]);
 //! 
 //! // The following attempts to move the slice beyond boundaries.
@@ -31,60 +35,76 @@
 //! # #[should_panic]
 //! # fn main() {
 //! # let mut arr = [1,2,3,4,5,6,7,8,9];
-//! let result = moveslice(&mut arr, (3,6), 7);
+//! arr.moveslice(3..6, 7); // will panic
 //! # }
+//! 
+//! // Panicking on failure however can prove to be not ideal.
+//! // If instead of panicking, you prefer a `Result`, use 
+//! // `try_moveslice`.
+//! let res = arr.try_moveslice(3..6, 7);
+//! assert!(res.is_err());
+//! 
+//! // Moveslice also comes with its own `Error` enum, since there 
+//! // are three main cases for failure.
 //! 
 //! // You could pass the destination as the same value as chunk.0.
 //! // However this would mean nothing is moved.
 //! // This doesn't panic, but it's a no-op.
-//! moveslice(&mut arr, (0,3), 0);
+//! arr.moveslice(0..3, 0);
 //! ```
+//! 
+//! [split-at-mut]: https://doc.rust-lang.org/std/primitive.slice.html#method.split_at_mut
+//! [rotate-left]: https://doc.rust-lang.org/std/primitive.slice.html#method.rotate_left
+//! [rotate-right]: https://doc.rust-lang.org/std/primitive.slice.html#method.rotate_right
 
-use std::ops::Bound::*;
-use std::ops::RangeBounds;
+use core::ops::Bound::*;
+use core::ops::RangeBounds;
 
-/// Moves a slice around in an array.
-/// Works by splitting and rotating.
+/// This Error enum has a single variant, which is used to return additional information about
+/// the out of bounds error, to help diagnostics.
 /// 
-/// There are three parameters:
-/// 
-/// - `slice` : The slice to modify.
-/// - `chunk` : A tuple with the boundaries of the chunk you want to move.
-/// - `destination` : Where you want to move the chunk.
-/// 
-/// Note that the destination specifies where the *first* element of the chunk 
-/// will be. As a result, its maximum value is not the length of the slice.
-/// 
-/// For example, if you have a slice with size 10, and you're moving a chunk of 
-/// size 3 around, the maximum value for the destination is *10-3=* ***7***.
-/// 
-/// # Panics
-/// Panics when the destination leads the chunk out of bounds.
-/// 
-/// In the example above, if I specify a destination of 8, the function will panic,
-/// showing what would be the placement of the chunk, and the length of the slice.
-/// 
-/// ```should_panic
-/// # use moveslice::moveslice;
-/// # fn main() {
-/// let mut arr = [1,2,3,4,5,6,7,8,9];
-/// let result = moveslice(&mut arr, (3,6), 7); // will panic
-/// # }
-/// ```
-
-
-pub trait Moveslice<T, R> {
-  type Target; 
-  type Err; 
-  fn moveslice(&mut self, bounds: R, destination: Self::Target)
-    where R: RangeBounds<usize>;
-  fn try_moveslice(&mut self, bounds: R, destination: Self::Target) -> Result<(), Self::Err>
-    where R: RangeBounds<usize>;
+/// Is used/returned by `try_moveslice`.
+#[derive(Debug)]
+pub enum Error {
+    /// This error signifies an out of bounds error.
+    /// It also contains the length of the slice, and 
+    /// the supposed location of where the chunk would have been.
+    /// 
+    /// For example:
+    /// `OutOfBoundsMove {len: 10, dest: (8,11)}`
+    OutOfBoundsMove {
+        /// The length of the array/slice being modified.
+        len: usize,
+        /// The location of where the chunk would have ended up.
+        dest: (usize, usize)
+    }
 }
 
+/// A trait declaring the `moveslice` and `try_moveslice` functions.
+/// Used to implement the functions on all slices. 
+pub trait Moveslice<T, R> {
+    /// Specifies the type of the destination index.
+    type Target; 
+
+    /// Specifies the errors being returned.
+    type Err;
+
+    /// Moves a slice within an array/slice around.
+    /// 
+    /// - `bounds` - specifies the range of where the subslice is. Examples: 3..5, 5..=8
+    /// - `destination` - specifies where the subslice should be moved to.
+    fn moveslice(&mut self, bounds: R, destination: Self::Target)
+        where R: RangeBounds<usize>;
+
+    /// Similar to `moveslice`, except it does not panic, returning a `Result` instead.
+    fn try_moveslice(&mut self, bounds: R, destination: Self::Target) -> Result<(), Self::Err>
+        where R: RangeBounds<usize>;
+}
+
+/// Implements the moveslice functions on all slices.
 impl<T: 'static,R,A> Moveslice<T,R> for A where A: AsMut<[T]> {
     type Target = usize;
-    type Err = String;
+    type Err = Error;
 
     fn moveslice(&mut self, bounds: R, destination: Self::Target)
     where R: RangeBounds<usize> 
@@ -92,20 +112,11 @@ impl<T: 'static,R,A> Moveslice<T,R> for A where A: AsMut<[T]> {
         let slice = self.as_mut();
         let startbound = bounds.start_bound();
         let endbound = bounds.end_bound();
-        let chunk = if let Included(x) = startbound {
-            if let Excluded(y) = endbound {
-                (*x,*y)
-            }
-            else if let Included(y) = endbound {
-                (*x,y+1)
-            }
-            else{
-                panic!("An endbound, excluded or included, is required.");
-            }
-        }
-        else {
-            panic!("A startbound is required.");
-        };
+        let x = if let Included(x) = startbound {*x} else {0};
+        let y = if let Excluded(x) = endbound {*x}
+                else if let Included(x) = endbound {x+1} 
+                else {slice.len()};
+        let chunk = (x,y);
 
         if destination > chunk.0 {
             let chunksize = chunk.1 - chunk.0;
@@ -114,11 +125,11 @@ impl<T: 'static,R,A> Moveslice<T,R> for A where A: AsMut<[T]> {
 
             let (_, mid) = slice.split_at_mut(index1);
 
-            let mid = if destination + chunksize <= mid.len() {
+            let mid = if index2 <= mid.len() {
                 mid.split_at_mut(index2).0
             } else {
                 panic!("Direction goes beyond slice [len = {}, destination = {}..{}]. ",
-                        mid.len(), destination, destination + chunksize);
+                        slice.len(), destination, destination + chunksize);
             };
 
             mid.rotate_left(chunk.1-chunk.0);
@@ -134,26 +145,17 @@ impl<T: 'static,R,A> Moveslice<T,R> for A where A: AsMut<[T]> {
         }
     }
 
-    fn try_moveslice(&mut self, bounds: R, destination: Self::Target) -> Result<(), Self::Err> 
+    fn try_moveslice(&mut self, bounds: R, destination: Self::Target) -> Result<(), Self::Err>
     where R: RangeBounds<usize> 
     {
         let slice = self.as_mut();
         let startbound = bounds.start_bound();
         let endbound = bounds.end_bound();
-        let chunk = if let Included(x) = startbound {
-            if let Excluded(y) = endbound {
-                (*x,*y)
-            }
-            else if let Included(y) = endbound {
-                (*x,y+1)
-            }
-            else{
-                return Err(String::from("An endbound, excluded or included, is required."));
-            }
-        }
-        else {
-            return Err(String::from("A startbound is required."));
-        };
+        let x = if let Included(x) = startbound {*x} else {0};
+        let y = if let Excluded(x) = endbound {*x}
+                else if let Included(x) = endbound {x+1} 
+                else {slice.len()};
+        let chunk = (x,y);
 
         if destination > chunk.0 {
             let chunksize = chunk.1 - chunk.0;
@@ -165,8 +167,10 @@ impl<T: 'static,R,A> Moveslice<T,R> for A where A: AsMut<[T]> {
             let mid = if destination + chunksize <= mid.len() {
                 mid.split_at_mut(index2).0
             } else {
-                return Err(format!("Direction goes beyond slice [len = {}, destination = {}..{}]. ",
-                        mid.len(), destination, destination + chunksize));
+                return Err(Error::OutOfBoundsMove {
+                    len: slice.len(),
+                    dest: (destination, destination + chunksize),
+                });
             };
 
             mid.rotate_left(chunk.1-chunk.0);
@@ -182,5 +186,5 @@ impl<T: 'static,R,A> Moveslice<T,R> for A where A: AsMut<[T]> {
         }
 
         Ok(())
-}
+    }
 }
